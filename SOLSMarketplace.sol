6d445d7d93628336dddd4af81bb6413a03572531
Software Engineer <im.ziad.sakr@gmail.com>
2023-04-16 04:35:12 -0700

    Update SOLSMarketplace.sol

--- SOLSMarketplace.sol
+++ SOLSMarketplace.sol
@@ -41,7 +41,7 @@ contract SOLSMarketplace is Ownable{
     event NFTListed(uint256) ;
 
     error PriceLow() ;
-
+    error TicketLow() ;
 
     constructor ( address _SOLT, address _LEGEN, address _RARE, address _BID, address _SOLN ) {
         SOLT = SOLSTOKEN(_SOLT);
@@ -97,8 +97,7 @@ contract SOLSMarketplace is Ownable{
         string memory uri
     ) external isValidProductId(product_id) isValidUnitId(_legen_param.product_unit) isValidUnitId(_legen_param.ticket_unit) onlyOwner  {
         if(_legen_param.product_price < 0) revert PriceLow();
-
-        require(_legen_param.ticket_price > 0, "Ticket price is too low");
+        if(_legen_param.ticket_price < 0) revert TicketLow();
         require(_legen_param.ticket_available > 0, "# of Tickets available should be bigger than 0") ;
         require(_legen_param.royalty >= 1, "Royalty should be bigger than 1") ;
 
